daa365a855a453cf90b39445ec7286d12b033977
MaheshJarati <MaheshJarati@IKCON1019>
2023-09-13 15:37:33 +0530

    Updated code for the business logic and added the delete employee functionality. Added the logger. Added the exception handling.

--- src/main/java/com/ikn/ums/employee/service/EmployeeService.java
+++ src/main/java/com/ikn/ums/employee/service/EmployeeService.java
@@ -11,8 +11,6 @@ public interface EmployeeService {
 	EmployeeVO fetchEmployeeDetailsWithDepartment(String email);
 	Employee saveEmployee(Employee employee);
 	EmployeeVO getEmployeeWithDepartment(Integer employeeId);
-	//get all UMS employee profiles
-	List<Employee> findAllEmployees();
 	//save all users by fetching details from organization azure active directory
 	Integer saveAzureUsers();
 	//save single user from azure
@@ -20,5 +18,6 @@ public interface EmployeeService {
 	//search employee count by their username
 	Integer searchEmployeeByEmail(String email);
 	void deleteEmployee(Integer employeeId);
+	List<Employee> getAllEmployees();
 
 }
